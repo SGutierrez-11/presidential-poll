04ee330cd13aa2a0c89af800a422e3b17d7febac
Santiago Gutiérrez <santicoguvi02@gmail.com>
2022-05-26 07:59:09 -0500

    Update page

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//Registrar
-
 const presidentialServices = require('./services/presidentialService');
 presidentialServices.createServices(app);
 
 
-//const PORT = process.env.PORT || 8080; 
+const PORT = process.env.PORT || 8080; 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log("El servidor esta preparado");
 });
